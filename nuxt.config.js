5cbce181f100c064c6032062738936cabcae64c0
iomcr <>
2022-02-13 15:51:03 -0500

    lintfix

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,17 +9,17 @@ export default {
   head: {
     title: 'randomnames',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -36,7 +36,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build'
+    '@nuxt/typescript-build',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -45,5 +45,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -2,31 +2,33 @@
   <main>
     <h2>
       Random Names
-      <button @click.prevent.stop="roll">Reroll</button>
+      <button @click.prevent.stop="roll">
+        Reroll
+      </button>
     </h2>
     <br>
     <p>{{ data.firstname }} {{ data.middlename }} {{ data.lastname }}</p>
-    <p v-text="data.jobtitle">
-    <p v-text="data.email">
-    <p v-text="data.address1">
-    <p v-text="data.address2">
+    <p v-text="data.jobtitle" />
+    <p v-text="data.email" />
+    <p v-text="data.address1" />
+    <p v-text="data.address2" />
     <p>{{ data.city }} {{ data.state }}, {{ data.zipcode }}</p>
   </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker'
 
 export default Vue.extend({
   name: 'IndexPage',
-  mounted() {
-    this.roll()
-  },
   data (): any {
     return {
-      data: {}
+      data: {},
     }
+  },
+  mounted () {
+    this.roll()
   },
   methods: {
     roll () {
